012e0545fc63c52a07da7dcecd4b43003321292b
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2021-05-03 14:47:08 +0800

    Add _redirect to use history push state

--- docs/_build.R
+++ docs/_build.R
@@ -5,3 +5,7 @@ bookdown::render_book("index.Rmd", "bookdown::epub_book", quiet = TRUE)
 # syntax highlighting and automatic linking of R code
 paths <- fs::dir_ls("_book", glob = "*.html")
 for (path in paths) downlit::downlit_html_path(path, path)
+
+# add _redirects to get History push state to work when deploying
+path <- fs::path("_book", "_redirects")
+writeLines("/* /index.html 200", path)
