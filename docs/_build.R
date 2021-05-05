59d4312ac14a82ff62c09b233f5fd26f7831a5e4
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2021-05-06 03:07:54 +0800

    Remove epub output

--- docs/_build.R
+++ docs/_build.R
@@ -1,11 +1,5 @@
-bookdown::render_book("index.Rmd", "bookdown::gitbook", quiet = TRUE)
-bookdown::render_book("index.Rmd", "bookdown::pdf_book", quiet = TRUE)
-bookdown::render_book("index.Rmd", "bookdown::epub_book", quiet = TRUE)
+bookdown::render_book("index.Rmd", quiet = TRUE)
 
 # syntax highlighting and automatic linking of R code
 paths <- fs::dir_ls("_book", glob = "*.html")
 for (path in paths) downlit::downlit_html_path(path, path)
-
-# add _redirects to get History push state to work when deploying
-path <- fs::path("_book", "_redirects")
-writeLines("/* /index.html 200", path)
